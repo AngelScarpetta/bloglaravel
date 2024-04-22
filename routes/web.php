fd1c7dd2cc0a39a55098c9e4f60eed0f5ffb4977
Angel Scarpetta <114451660+angel1456s@users.noreply.github.com>
2024-04-22 12:39:36 -0500

    blog con laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+Route::get('curso', [CursoController::class, 'index'] );
+Route::get('curso/create', [CursoController::class, 'create']);
+Route::get('curso/{curso}', [CursoController::class, 'show']);
